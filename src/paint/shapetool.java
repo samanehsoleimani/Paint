5d381bcd1bc0992ee126bac7015bea4afdf23ab0
samanehsoleimani <soleimanisaman08@gmail.com>
2026-06-29 23:56:45 +0430

    Add shapetool class to Paint project

---
+++ src/paint/shapetool.java
@@ -0,0 +1,9 @@
+package paint;
+
+	public class shapetool {
+	    public static final int Line = 1;
+	    public static final int Rect = 2;
+	    public static final int Oval = 3;
+	    public static final int eraser = 5; 
+	    public static final int pencil = 4; 
+	}
